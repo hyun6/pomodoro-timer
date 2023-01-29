3413af9acbb9f0f85d1a837056fd45eddb090722
hyun6 <psymhs@gmail.com>
2023-01-29 22:11:41 +0900

    remove: counter example code

--- test/app/view/app_test.dart
+++ test/app/view/app_test.dart
@@ -1,12 +1,12 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:pomodoro_timer/app/app.dart';
-import 'package:pomodoro_timer/counter/counter.dart';
+import 'package:pomodoro_timer/timer/view/timer_page.dart';
 
 void main() {
   group('App', () {
     testWidgets('renders CounterPage', (tester) async {
       await tester.pumpWidget(const App());
-      expect(find.byType(CounterPage), findsOneWidget);
+      expect(find.byType(TimerPage), findsOneWidget);
     });
   });
 }
