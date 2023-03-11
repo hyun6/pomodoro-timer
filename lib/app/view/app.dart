5cf4557434601dae4c658a63da39c732d210592c
hyun6 <psymhs@gmail.com>
2023-03-11 22:45:54 +0900

    feat(theme): using dark theme

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -17,6 +17,8 @@ class App extends StatelessWidget {
         ),
         useMaterial3: true,
       ),
+      darkTheme: ThemeData.dark(useMaterial3: true),
+      themeMode: ThemeMode.dark,
       localizationsDelegates: AppLocalizations.localizationsDelegates,
       supportedLocales: AppLocalizations.supportedLocales,
       home: BlocProvider(
