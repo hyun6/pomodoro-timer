b1abd2cda01b25b42cf782f289c894cc4a50cfe5
hyun6 <psymhs@gmail.com>
2023-09-18 17:25:13 +0900

    fix: init audio volume from saved settings value

--- lib/app/view/app_page.dart
+++ lib/app/view/app_page.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:get_it/get_it.dart';
 import 'package:pomodoro_timer/app/view/platform_menu.dart';
 import 'package:pomodoro_timer/l10n/l10n.dart';
+import 'package:pomodoro_timer/services/audio_service.dart';
 import 'package:pomodoro_timer/services/material_context.dart';
 import 'package:pomodoro_timer/settings/cubit/settings_cubit.dart';
 import 'package:pomodoro_timer/timer/view/timer_page.dart';
@@ -12,7 +14,12 @@ class AppPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
-      create: (_) => SettingsCubit(),
+      create: (_) {
+        final settingsCubit = SettingsCubit();
+        GetIt.instance<AudioService>()
+            .setVolume(settingsCubit.state.settings.volume / 100);
+        return settingsCubit;
+      },
       child: MaterialApp(
         theme: ThemeData(
           appBarTheme: const AppBarTheme(color: Color(0xFFFFFFFF)),
